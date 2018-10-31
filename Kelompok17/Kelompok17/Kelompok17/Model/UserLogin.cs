b4577be57b6aff171dad929998920b110124779b
Fahmi M Mochtar <fmmochtar@student.undip.ac.id>
2018-10-31 15:15:01 +0700

    Kelompok 17 MDP Modul 1
    Kelompok 17 MDP Modul 1

---
+++ Kelompok17/Kelompok17/Kelompok17/Model/UserLogin.cs
@@ -0,0 +1,8 @@
+﻿namespace Kelompok17
+{
+    public static class UserLogin
+    {
+        public static string Username = "Kelompok17";
+        public static string Password = "password";
+    }
+}
---
+++ Kelompok17/Kelompok17/Kelompok17/View/HalamanLihatData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Kelompok17.Model;
+using System.IO;
+using SQLite;
+
+using Xamarin.Forms;
+
+namespace Kelompok17.View
+{
+	public class HalamanLihatData : ContentPage
+	{
+        private ListView _listview;
+        string _dbPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "myDB.db4");
+        public HalamanLihatData ()
+		{
+            this.Title = "Data Mahasiswa";
+
+            var db = new SQLiteConnection(_dbPath);
+            StackLayout stackLayout = new StackLayout();
+
+            _listview = new ListView();
+            _listview.ItemsSource = db.Table<DataMahasiswa>().OrderBy(x => x.Nama).ToList();
+            stackLayout.Children.Add(_listview);
+
+            Content = stackLayout;
+
+			}
+		}
+	}
